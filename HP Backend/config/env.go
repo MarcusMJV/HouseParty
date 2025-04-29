b33ad96f66b5dd69724c0c679f40726dcc7fada5
MarcusMJV <vorstermarcus@gmail.com>
2025-04-29 18:10:02 +0200

    fixing docker

--- HP Backend/config/env.go
+++ HP Backend/config/env.go
@@ -1,15 +1,16 @@
 package config
 
 import (
-	"os"
 	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
 	err := godotenv.Load()
-	if err != nil{
-		log.Fatal("error loading .env file")
+	if err != nil {
+		log.Println("no .env file loaded")
 	}
 }
 
